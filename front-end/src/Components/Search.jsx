6e9abc2e2239194ad5289864e51f646c135375ea
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-03 11:39:55 -0300

    Feat: Criando o search como componente para deixar o codigo mais limpo

---
+++ front-end/src/Components/Search.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Input from './Input';
+import Button from './Button';
+
+function Search({ resultApi, api, setResultApi }) {
+  const [search, setSearch] = useState('');
+  const [type, setType] = useState('');
+
+  const getRadioSelected = (e) => {
+    setType(e.target.value);
+  };
+
+  const handleClick = (typ) => {
+    if (typ === 'name') {
+      const filtro = [...resultApi].filter((item) => (
+        item.name.first.includes(search)
+      ));
+      setResultApi(filtro);
+      setSearch('');
+    } else if (typ === 'email') {
+      const filtro = [...resultApi].filter((item) => (
+        item.email.includes(search)
+      ));
+      setResultApi(filtro);
+      setSearch('');
+    } else {
+      api();
+      setSearch('');
+    }
+  };
+
+  return (
+    <div className=" mb-4">
+      <Input
+        type="text"
+        name="Pesquisar por nome ou e-mail"
+        handleChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      <div className="m-2 d-flex justify-content-between" onChange={(e) => getRadioSelected(e)}>
+        <label htmlFor="name">
+          <input
+            type="radio"
+            id="name"
+            name="radioButton"
+            className="form-check-input me-2"
+            defaultChecked={type === 'name'}
+            value="name"
+          />
+          Nome
+        </label>
+        <label htmlFor="email">
+          <input
+            type="radio"
+            id="email"
+            name="radioButton"
+            className="form-check-input me-2"
+            defaultChecked={type === 'email'}
+            value="email"
+          />
+          E-mail
+        </label>
+        <label htmlFor="clear">
+          <input
+            type="radio"
+            id="clear"
+            name="radioButton"
+            className="form-check-input me-2"
+            defaultChecked={type === 'clear'}
+            value="clear"
+          />
+          Limpar filtro
+        </label>
+      </div>
+      <Button
+        click={() => handleClick(type)}
+        dataTestId="btn-pesquisar"
+        sty="w-100"
+      >
+        Pesquisar
+      </Button>
+    </div>
+  );
+}
+
+Search.propTypes = {
+  resultApi: PropTypes.arrayOf.isRequired,
+  api: PropTypes.func.isRequired,
+  setResultApi: PropTypes.func.isRequired,
+};
+
+export default Search;
