35ea79204f8461ebc5c827e03aaa27dd58063c51
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-03 11:16:04 -0300

    Feat: Criando o filtro por nome e email

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import RandonUsers from '../Components/RandonUsers';
 import randonUser from '../Services/Request';
+import Input from '../Components/Input';
+import Button from '../Components/Button';
 
 function Home() {
   const [resultApi, setResultApi] = useState([]);
   const [itensPerPage, setItensPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(0);
+  const [search, setSearch] = useState('');
+  const [type, setType] = useState('');
 
   const pages = Math.ceil(resultApi.length / itensPerPage);
   const startIndex = currentPage * itensPerPage;
@@ -17,6 +21,10 @@ function Home() {
     setResultApi(result);
   };
 
+  const getRadioSelected = (e) => {
+    setType(e.target.value);
+  };
+
   useEffect(() => {
     api();
   }, []);
@@ -25,9 +33,72 @@ function Home() {
     setCurrentPage(0);
   }, [itensPerPage]);
 
+  const handleClick = (typ) => {
+    if (typ === 'name') {
+      const filtro = [...resultApi].filter((item) => (
+        item.name.first.includes(search)
+      ));
+      setResultApi(filtro);
+      setSearch('');
+    } else if (typ === 'email') {
+      const filtro = [...resultApi].filter((item) => (
+        item.email.first.includes(search)
+      ));
+      setResultApi(filtro);
+      setSearch('');
+    } else {
+      api();
+      setSearch('');
+    }
+  };
+
   return (
-    <main>
-      <div className="d-flex justify-content-between container mt-2">
+    <main className="container">
+      <div className="d-flex justify-content-between mb-4">
+        <Input
+          type="text"
+          name="Pesquisar por nome ou e-mail"
+          handleChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <div className="m-2" onChange={(e) => getRadioSelected(e)}>
+          <label htmlFor="name">
+            <input
+              type="radio"
+              id="name"
+              name="radioButton"
+              className="form-check-input"
+              defaultChecked={type === 'name'}
+              value="name"
+            />
+            Nome
+          </label>
+          <label htmlFor="email">
+            <input
+              type="radio"
+              id="email"
+              name="radioButton"
+              className="form-check-input"
+              defaultChecked={type === 'email'}
+              value="email"
+            />
+            E-mail
+          </label>
+          <label htmlFor="clear">
+            <input
+              type="radio"
+              id="clear"
+              name="radioButton"
+              className="form-check-input"
+              defaultChecked={type === 'clear'}
+              value="clear"
+            />
+            Limpar filtro
+          </label>
+        </div>
+        <Button click={() => handleClick(type)} dataTestId="btn-pesquisar">Pesquisar</Button>
+      </div>
+      <div className="d-flex justify-content-between mt-2">
         <div>Foto</div>
         <div>Nome</div>
         <div>email</div>
