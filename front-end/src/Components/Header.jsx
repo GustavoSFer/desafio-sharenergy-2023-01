f5bdc432b3ac4ba720c0fd6aeb02a7bb5ac9661b
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-04 14:53:49 -0300

    Feat: Melhorando o header

--- front-end/src/Components/Header.jsx
+++ front-end/src/Components/Header.jsx
@@ -2,26 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const logoff = () => {
+    localStorage.removeItem('user');
+  };
+
   return (
-    <header className="text-end mt-4 border border-success p-3 rounded bg-primary">
-      <Link
-        to="/home"
-        className="m-4 text-light text-decoration-none fw-bolder"
-      >
-        Home
-      </Link>
-      <Link
-        to="/cats"
-        className="m-4 text-light text-decoration-none fw-bolder"
-      >
-        Cats
-      </Link>
-      <Link
-        to="/dogs"
-        className="m-4 text-light text-decoration-none fw-bolder"
-      >
-        Dogs
-      </Link>
+    <header
+      className="mt-4 border border-success p-3 rounded bg-primary d-flex justify-content-between"
+    >
+      <div>
+        <Link
+          to="/home"
+          className="m-4 text-light text-decoration-none fw-bolder"
+        >
+          Users
+        </Link>
+        <Link
+          to="/cats"
+          className="m-4 text-light text-decoration-none fw-bolder"
+        >
+          Cats
+        </Link>
+        <Link
+          to="/dogs"
+          className="m-4 text-light text-decoration-none fw-bolder"
+        >
+          Dogs
+        </Link>
+      </div>
+      <div>
+        <Link
+          to="/"
+          onClick={logoff}
+          className="m-4 text-light text-decoration-none fw-bolder"
+        >
+          Logoff
+        </Link>
+      </div>
     </header>
   );
 }
